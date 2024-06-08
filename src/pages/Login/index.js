6a97b9dc86df024c3c0bbe7dbdd2a29943828c02
Samuel Dassoler <55854831+srodrigues51@users.noreply.github.com>
2024-06-08 18:23:27 -0300

    Update pagina login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -19,7 +19,7 @@ function Login() {
           <label>Informe sua senha</label>
           <input type="password" id="lpassword" name="lpassword"  />
         </form>
-        <button className="waves-effect waves-light btn center" > Login </button>
+        <button className="waves-effect waves-light btn center" ><Link to="/home" className="waves-effect waves-light btn-large">Logi</Link>  </button>
       </div>
     </div>
 
